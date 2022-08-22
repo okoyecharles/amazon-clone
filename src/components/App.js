18df9cd73809c259d798db9f61e3e50775459a8d
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-22 15:55:14 +0100

    Setup Files

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build the amazon clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBcfkwzoNETXIIKxZ9moMbjjE_9cs3AelA",
+  authDomain: "clone-f50ae.firebaseapp.com",
+  projectId: "clone-f50ae",
+  storageBucket: "clone-f50ae.appspot.com",
+  messagingSenderId: "644834230684",
+  appId: "1:644834230684:web:c96208257b60fa685cc859",
+  measurementId: "G-LZX5ZYX515"
+};
